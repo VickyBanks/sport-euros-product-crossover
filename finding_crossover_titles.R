b46b5c0f9a5cbeb970b6dcd0f46662e8760b143d
Vicky Banks <victoria.banks052@gmail.com>
2021-06-23 11:55:14 +0100

    Found the top 10 programme titles for each user group across the three products

--- finding_crossover_titles.R
+++ finding_crossover_titles.R
@@ -22,7 +22,6 @@ rm(redshift_username, user, redshift_password, password)
 
 
 ####### get data
-
 vb_euros_crossover<- dbGetQuery(conn, "SELECT * FROM central_insights_sandbox.vb_euros_crossover;")
 vb_euros_iplayer<- dbGetQuery(conn, "SELECT * FROM central_insights_sandbox.vb_euros_iplayer;")
 vb_euros_sounds<- dbGetQuery(conn, "SELECT * FROM central_insights_sandbox.vb_euros_sounds;")
@@ -32,41 +31,81 @@ product_groups<-vb_euros_crossover%>%select(products_used) %>%distinct()%>%arran
 product_groups 
 
 rank<- data.frame(rank = c(1,2,3,4,5,6,7,8,9,10))
+#make empty DF
+top_content <- data.frame(
+  products_user = character(),
+  rank = double(),
+  iplayer_title  = character(),
+  iplayer_users = integer(),
+  sounds_title  = character(),
+  sounds_users = integer(),
+  sport_title  = character(),
+  sport_users = integer()
+)
 
-iplayer_titles<-
-vb_euros_crossover %>%
-  filter(products_used == '2_sport_iplayer') %>%
-  select(audience_id)%>%
+for(row in 1:nrow(product_groups)){
+##find top titles
+iplayer_titles <-
+  vb_euros_crossover %>%
+  filter(products_used == product_groups[row,]) %>%
+  select(audience_id) %>%
   left_join(vb_euros_iplayer, by = 'audience_id') %>%
-  select(-age_group)%>%
-  group_by(programme_title)%>%
-  count()%>%
-  rename(users = n)%>%
-  rename(iplayer_title = programme_title)%>%
-  arrange(desc(users)) %>% head(n=10)
+  select(-age_group) %>%
+  group_by(programme_title) %>%
+  count() %>%
+  rename(iplayer_users = n) %>%
+  rename(iplayer_title = programme_title) %>%
+  arrange(desc(iplayer_users)) %>% 
+  head(n = 10) %>%
+  mutate(products_used = product_groups[row,]) %>%
+  cbind(rank)%>%
+  select(products_used, rank, iplayer_title, iplayer_users)
+
+
 
 sounds_titles<-
   vb_euros_crossover %>%
-  filter(products_used == '2_sport_iplayer') %>%
+  filter(products_used == product_groups[row,]) %>%
   select(audience_id)%>%
   left_join(vb_euros_sounds, by = 'audience_id') %>%
   select(-age_group)%>%
   group_by(programme_title)%>%
   count()%>%
-  rename(users = n)%>%
+  rename(sounds_users = n)%>%
   rename(sounds_title = programme_title)%>%
-  arrange(desc(users)) %>% head(n=10)
+  arrange(desc(sounds_users)) %>% head(n=10)
 
 sport_titles<-
   vb_euros_crossover %>%
-  filter(products_used == '2_sport_iplayer') %>%
+  filter(products_used == product_groups[row,]) %>%
   select(audience_id)%>%
   left_join(vb_euros_sport, by = 'audience_id') %>%
   select(-age_group)%>%
   group_by(programme_title)%>%
   count()%>%
-  rename(users = n)%>%
+  rename(sport_users = n)%>%
   rename(sport_title = programme_title)%>%
-  arrange(desc(users))%>% head(n=10)
+  arrange(desc(sport_users))%>% head(n=10)
 
-rank %>%cbind(sport_titles) %>%cbind(iplayer_titles)%>%cbind(sounds_titles)
+group_combined<-iplayer_titles %>% cbind(sounds_titles)%>%cbind(sport_titles)
+
+## If there are no titles because users didn't visit, remove the user count as it's confusing
+group_combined$iplayer_users[is.na(group_combined$iplayer_title)] <- NA
+group_combined$sounds_users[is.na(group_combined$sounds_title)] <- NA
+group_combined$sport_users[is.na(group_combined$sport_title)] <- NA
+
+top_content<-top_content %>%rbind(group_combined)
+}
+top_content %>%View()
+
+getwd()
+write.csv(top_content, "top_content_items.csv", row.names = FALSE)
+
+
+### re-cut the data to make comparisons easier
+
+
+
+
+
+
